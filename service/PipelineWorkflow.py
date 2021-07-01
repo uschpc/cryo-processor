5fee84a6d59c5e51f2d7ce5cede593bf3cc42521
osinskit <osinskit@gmail.com>
2021-07-01 12:43:56 -0700

    Update PipelineWorkflow.py

--- service/PipelineWorkflow.py
+++ service/PipelineWorkflow.py
@@ -201,6 +201,15 @@ class PipelineWorkflow:
                                      memory="2048"
         ).add_profiles(Namespace.PEGASUS, key="clusters.size", value=cluster_size)
 
+        magick = Transformation(
+            "magick",
+            site=exec_site_name,
+            pfn=os.path.join(self.base_dir, "workflow/scripts/imagemagick_wrapper.sh"),
+            is_stageable=False
+        )
+        magick.add_pegasus_profile( cores="1",
+                                        runtime="180"
+        )
         
         self.tc.add_transformations(dm2mrc_gainref)
         self.tc.add_transformations(newstack_gainref)
@@ -211,6 +220,7 @@ class PipelineWorkflow:
         self.tc.add_transformations(motionCor2)
         self.tc.add_transformations(gctf)
         self.tc.add_transformations(e2proc2d)
+        self.tc.add_transformations(magick)
 
     # --- Replica Catalog ------------------------------------------------------
     def create_replica_catalog(self,exec_site_name="slurm"):
@@ -227,82 +237,85 @@ class PipelineWorkflow:
         #mc_prefix = "mc_"
         
         
-        
+        #Try to find Gain reference file - it might not be a part of the dataset, 
+        #so we must take it into account.
         #define Gain reference Super resolution input and output filename
         Raw_Gain_Ref_SR_path = self.find_files2(self.inputs_dir, self.rawgainref)[0]
-        Raw_Gain_Ref_SR_name = os.path.basename(Raw_Gain_Ref_SR_path)
-        Raw_Gain_Ref_SR = File(Raw_Gain_Ref_SR_name)
-        #TODO: improve lines below
-        Gain_Ref_SR_path = Raw_Gain_Ref_SR_path.replace('x1.m1.dm4','_SuperRes.x1.m1.mrc')
-        Gain_Ref_SR_name = os.path.basename(Gain_Ref_SR_path)
-        Gain_Ref_SR = File(Gain_Ref_SR_name)
-        self.rc.add_replica("slurm", Raw_Gain_Ref_SR_name, "file://{}".format(Raw_Gain_Ref_SR_path))
+        if len(Raw_Gain_Ref_SR_path) != 0:
+            Raw_Gain_Ref_SR_name = os.path.basename(Raw_Gain_Ref_SR_path)
+            Raw_Gain_Ref_SR = File(Raw_Gain_Ref_SR_name)
+            #TODO: improve lines below
+            Gain_Ref_SR_path = Raw_Gain_Ref_SR_path.replace('x1.m1.dm4','_SuperRes.x1.m1.mrc')
+            Gain_Ref_SR_name = os.path.basename(Gain_Ref_SR_path)
+            Gain_Ref_SR = File(Gain_Ref_SR_name)
+            self.rc.add_replica("slurm", Raw_Gain_Ref_SR_name, "file://{}".format(Raw_Gain_Ref_SR_path))
         
-        #define Gain reference output filename
-        #TODO: improve lines below
-        Gain_Ref_path = Gain_Ref_SR_path.replace('_SuperRes.x1.m1.mrc','_std.x1.m1.mrc')
-        Gain_Ref_name = os.path.basename(Gain_Ref_path)
-        Gain_Ref = File(Gain_Ref_name)
+            #define Gain reference output filename
+            #TODO: improve lines below
+            Gain_Ref_path = Gain_Ref_SR_path.replace('_SuperRes.x1.m1.mrc','_std.x1.m1.mrc')
+            Gain_Ref_name = os.path.basename(Gain_Ref_path)
+            Gain_Ref = File(Gain_Ref_name)
         
-        #define flip Y Super resolution output filename
-        #TODO: improve lines below
-        FlipY_SR_path = Gain_Ref_SR_path.replace('_SuperRes.x1.m1.mrc','_sr.flipy.x1.m1.mrc')
-        #logger.info(" ... found {} ".format(FlipY_SR_path))
-        FlipY_SR_name = os.path.basename(FlipY_SR_path)
-        FlipY_SR = File(FlipY_SR_name)
+            #define flip Y Super resolution output filename
+            #TODO: improve lines below
+            FlipY_SR_path = Gain_Ref_SR_path.replace('_SuperRes.x1.m1.mrc','_sr.flipy.x1.m1.mrc')
+            #logger.info(" ... found {} ".format(FlipY_SR_path))
+            FlipY_SR_name = os.path.basename(FlipY_SR_path)
+            FlipY_SR = File(FlipY_SR_name)
         
-        #define flip Y std resolution output filename
-        #TODO: improve lines below
-        FlipY_path = Gain_Ref_path.replace('_std.x1.m1.mrc','_std.flipy.x1.m1.mrc')
-        FlipY_name = os.path.basename(FlipY_path)
-        FlipY = File(FlipY_name)
+            #define flip Y std resolution output filename
+            #TODO: improve lines below
+            FlipY_path = Gain_Ref_path.replace('_std.x1.m1.mrc','_std.flipy.x1.m1.mrc')
+            FlipY_name = os.path.basename(FlipY_path)
+            FlipY = File(FlipY_name)
+            #convert Superres dm4 file to mrc
+            #dm2mrc usage: dm2mrc infile outfile
+            dm2mrc_gainref_sr_job = Job("dm2mrc_gainref")
+            dm2mrc_gainref_sr_job.add_args(Raw_Gain_Ref_SR, Gain_Ref_SR)
+            dm2mrc_gainref_sr_job.add_inputs(Raw_Gain_Ref_SR)
+            dm2mrc_gainref_sr_job.add_outputs(Gain_Ref_SR, stage_out=True)
+            #create standard resolution gain ref file from superres gain ref file
+            #newstack usage here (decrease the size of Super resolution image by factor of 2): newstack -bin 2 infile outfile
+            newstack_gainref_job = Job("newstack_gainref")
+            newstack_gainref_job.add_args("-bin", "2", Gain_Ref_SR, Gain_Ref)
+            newstack_gainref_job.add_inputs(Gain_Ref_SR)
+            newstack_gainref_job.add_outputs(Gain_Ref, stage_out=True)
+            #flip both gain reference files on y axis
+            #clip usage here (flip img on Y axis): clip flipy infile outfile
+            #std resolution
+            clip_gainref_job = Job("clip_gainref")
+            clip_gainref_job.add_args("flipy", Gain_Ref, FlipY)
+            clip_gainref_job.add_inputs(Gain_Ref)
+            clip_gainref_job.add_outputs(FlipY, stage_out=True)
+            #super resolution
+            clip_gainref_superres_job = Job("clip_gainref_superres")
+            clip_gainref_superres_job.add_args("flipy", Gain_Ref_SR, FlipY_SR)
+            clip_gainref_superres_job.add_inputs(Gain_Ref_SR)
+            clip_gainref_superres_job.add_outputs(FlipY_SR, stage_out=True)
+            self.wf.add_jobs(dm2mrc_gainref_sr_job)
+            self.wf.add_jobs(newstack_gainref_job)
+            self.wf.add_jobs(clip_gainref_job)
+            self.wf.add_jobs(clip_gainref_superres_job)
         
+        #Try to find Defect Map file - it might not be a part of the dataset, 
+        #so we must take it into account.        
         #define Defect Map input and output filename
         Raw_Defect_Map_path = self.find_files2(self.inputs_dir, self.rawdefectsmap)[0]
-        Raw_Defect_Map_name = os.path.basename(Raw_Defect_Map_path)
-        Raw_Defect_Map = File(Raw_Defect_Map_name)
-        Defect_Map_path = Raw_Defect_Map_path.replace('.dm4','.mrc')
-        Defect_Map_name = os.path.basename(Defect_Map_path)
-        Defect_Map = File(Defect_Map_name)
-        self.rc.add_replica("slurm", Raw_Defect_Map_name, "file://{}".format(Raw_Defect_Map_path))
-        
-        #convert Superres dm4 file to mrc
-        #dm2mrc usage: dm2mrc infile outfile
-        dm2mrc_gainref_sr_job = Job("dm2mrc_gainref")
-        dm2mrc_gainref_sr_job.add_args(Raw_Gain_Ref_SR, Gain_Ref_SR)
-        dm2mrc_gainref_sr_job.add_inputs(Raw_Gain_Ref_SR)
-        dm2mrc_gainref_sr_job.add_outputs(Gain_Ref_SR, stage_out=True)
-        #create standard resolution gain ref file from superres gain ref file
-        #newstack usage here (decrease the size of Super resolution image by factor of 2): newstack -bin 2 infile outfile
-        newstack_gainref_job = Job("newstack_gainref")
-        newstack_gainref_job.add_args("-bin", "2", Gain_Ref_SR, Gain_Ref)
-        newstack_gainref_job.add_inputs(Gain_Ref_SR)
-        newstack_gainref_job.add_outputs(Gain_Ref, stage_out=True)
-        #flip both gain reference files on y axis
-        #clip usage here (flip img on Y axis): clip flipy infile outfile
-        #std resolution
-        clip_gainref_job = Job("clip_gainref")
-        clip_gainref_job.add_args("flipy", Gain_Ref, FlipY)
-        clip_gainref_job.add_inputs(Gain_Ref)
-        clip_gainref_job.add_outputs(FlipY, stage_out=True)
-        #super resolution
-        clip_gainref_superres_job = Job("clip_gainref_superres")
-        clip_gainref_superres_job.add_args("flipy", Gain_Ref_SR, FlipY_SR)
-        clip_gainref_superres_job.add_inputs(Gain_Ref_SR)
-        clip_gainref_superres_job.add_outputs(FlipY_SR, stage_out=True)
-        #we do this, but if it impacts the processing speed to much it can be disabled for now
-        #create defect map file
-        #dm2mrc usage: dm2mrc infile outfile
-        dm2mrc_defect_map_job = Job("dm2mrc_defect_map")
-        dm2mrc_defect_map_job.add_args(Raw_Defect_Map, Defect_Map)
-        dm2mrc_defect_map_job.add_inputs(Raw_Defect_Map)
-        dm2mrc_defect_map_job.add_outputs(Defect_Map, stage_out=True)
-        
-        self.wf.add_jobs(dm2mrc_gainref_sr_job)
-        self.wf.add_jobs(newstack_gainref_job)
-        self.wf.add_jobs(clip_gainref_job)
-        self.wf.add_jobs(clip_gainref_superres_job)
-        self.wf.add_jobs(dm2mrc_defect_map_job)
+        if len(Raw_Gain_Ref_SR_path) != 0:
+            Raw_Defect_Map_name = os.path.basename(Raw_Defect_Map_path)
+            Raw_Defect_Map = File(Raw_Defect_Map_name)
+            Defect_Map_path = Raw_Defect_Map_path.replace('.dm4','.mrc')
+            Defect_Map_name = os.path.basename(Defect_Map_path)
+            Defect_Map = File(Defect_Map_name)
+            self.rc.add_replica("slurm", Raw_Defect_Map_name, "file://{}".format(Raw_Defect_Map_path))
+            #we do this, but if it impacts the processing speed to much it can be disabled for now
+            #create defect map file
+            #dm2mrc usage: dm2mrc infile outfile
+            dm2mrc_defect_map_job = Job("dm2mrc_defect_map")
+            dm2mrc_defect_map_job.add_args(Raw_Defect_Map, Defect_Map)
+            dm2mrc_defect_map_job.add_inputs(Raw_Defect_Map)
+            dm2mrc_defect_map_job.add_outputs(Defect_Map, stage_out=True)
+            self.wf.add_jobs(dm2mrc_defect_map_job)
         
         
         # for each _fractions.(tiff|mrc) in the Images-Disc1 dir 
@@ -317,6 +330,7 @@ class PipelineWorkflow:
             file_list = random.sample(file_list, 10)
         else:
             # even for production, only process a part of the dataset (maybe change this later?)
+            #
             file_list = random.sample(file_list, 1000)
 
         for fraction_file_path in file_list:
@@ -333,13 +347,13 @@ class PipelineWorkflow:
             mrc_file = File(mrc_file_name)
             dw_file = File(dw_file_name)
 
-            #find nad copy the jpeg file 
+            #find and copy the jpeg file 
             jpeg_file_path_dirname=os.path.dirname(fraction_file_path)
             jpeg_file_name=("%s.jpg"%"_".join(fraction_file_name.split("_")[:-1]))
             jpeg_file_path=os.sep.join([jpeg_file_path_dirname,jpeg_file_name])
             # use a "fake" input lfn and a real output lfn
             jpeg_file = File(jpeg_file_name + "-IN")
-            jpeg_file_out = File(jpeg_file_name)
+            jpeg_file_out = File(jpeg_file_name.replace(".jpg","_raw.jpg"))
             self.rc.add_replica("slurm", jpeg_file_name + "-IN", "file://{}".format(jpeg_file_path))
             copy_jpeg_job = Job("copy_jpeg").add_args("-v", "-L", "./{}-IN".format(jpeg_file_name), jpeg_file_out)
             copy_jpeg_job.add_inputs(jpeg_file)
@@ -359,10 +373,16 @@ class PipelineWorkflow:
                 logger.info("Unknown image extension - %s"%self.basename_extension)
                 sys.exit(1)
             
-            motionCor_job = Job("MotionCor2").add_args(mc2_in, "./{}".format(fraction_file_name), "-OutMrc",
-                mrc_file, "-Gain", FlipY,"-Iter 7 -Tol 0.5 -RotGain 2",
-                "-PixSize", self.apix, "-FmDose", self.fmdose, "-Throw", self.throw, "-Trunc", self.trunc, "-Gpu 0 1 -Serial 0",
-                "-OutStack 0", "-SumRange 0 0")
+            if len(Raw_Gain_Ref_SR_path) != 0:
+                motionCor_job = Job("MotionCor2").add_args(mc2_in, "./{}".format(fraction_file_name), "-OutMrc",
+                    mrc_file, "-Gain", FlipY,"-Iter 7 -Tol 0.5 -RotGain 2",
+                    "-PixSize", self.apix, "-FmDose", self.fmdose, "-Throw", self.throw, "-Trunc", self.trunc, "-Gpu 0 1 -Serial 0",
+                    "-OutStack 0", "-SumRange 0 0")
+            else:
+                motionCor_job = Job("MotionCor2").add_args(mc2_in, "./{}".format(fraction_file_name), "-OutMrc",
+                    mrc_file, "-Iter 7 -Tol 0.5 -RotGain 2",
+                    "-PixSize", self.apix, "-FmDose", self.fmdose, "-Throw", self.throw, "-Trunc", self.trunc, "-Gpu 0 1 -Serial 0",
+                    "-OutStack 0", "-SumRange 0 0")
 
             motionCor_job.add_inputs(fraction_file, FlipY)
             motionCor_job.add_outputs(mrc_file, stage_out=False, register_replica=False)
@@ -382,21 +402,38 @@ class PipelineWorkflow:
             )
 
             gctf_job.add_inputs(mrc_file)
-            gctf_job.add_args(mrc_file)
+            #gctf_job.add_args(mrc_file) # i guess it should be removed
             gctf_job.add_outputs(ctf_star_file, stage_out=True, register_replica=True)
             #gctf_job.add_outputs(ctf_pf_file, stage_out=True, register_replica=True)
             gctf_job.add_outputs(ctf_file, stage_out=True, register_replica=True)
             gctf_job.add_outputs(gctf_log_file, stage_out=True, register_replica=True)
-
-            # e2proc2d            
-            jpg_ctf_file = File(mrc_file_name.replace(".mrc","_ctf.jpg"))
-            e2proc2d_job1 = Job("e2proc2d")            
-            e2proc2d_job1.add_inputs(ctf_file)
-            e2proc2d_job1.add_outputs(jpg_ctf_file, stage_out=True)
-            e2proc2d_job1.add_args(ctf_file, jpg_ctf_file)
-
             self.wf.add_jobs(gctf_job)
+
+            # e2proc2d - motion-corrected to jpg, then resize to 20% size
+            dw_jpg_name = dw_file_name.replace("_DW.mrc","_DW_fs.jpg")
+            dw_jpg_file = File(dw_jpg_name)
+            e2proc2d_job1 = Job("e2proc2d")            
+            e2proc2d_job1.add_inputs(dw_file)
+            e2proc2d_job1.add_outputs(dw_jpg_file, stage_out=True)
+            e2proc2d_job1.add_args(dw_file, dw_jpg_file)
             self.wf.add_jobs(e2proc2d_job1)
+            magick_jpg_file = File(dw_jpg_name.replace("_DW_fs.jpg","_DW.jpg"))
+            magick_resize = Job("magick")
+            magick_resize.add_inputs(dw_jpg_file)
+            magick_resize.add_outputs(magick_jpg_file, stage_out=True, register_replica=True)
+            magick_resize.add_args("convert -resize 20%", dw_jpg_file, magick_jpg_file)
+            self.wf.add_jobs(magick_resize)
+            
+            
+            # e2proc2d - ctf to jpg
+            jpg_ctf_file = File(mrc_file_name.replace(".mrc","_ctf.jpg"))
+            e2proc2d_job2 = Job("e2proc2d")            
+            e2proc2d_job2.add_inputs(ctf_file)
+            e2proc2d_job2.add_outputs(jpg_ctf_file, stage_out=True)
+            e2proc2d_job2.add_args(ctf_file, jpg_ctf_file)
+            self.wf.add_jobs(e2proc2d_job2)
+            
+            
 
 
     def set_params(self, datum):
