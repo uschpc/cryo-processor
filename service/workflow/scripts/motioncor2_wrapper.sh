790565b4f31ee95060c19bd682e911be38cf1190
osinskit <osinskit@gmail.com>
2021-07-06 15:12:57 -0700

    Update motioncor2_wrapper.sh

--- service/workflow/scripts/motioncor2_wrapper.sh
+++ service/workflow/scripts/motioncor2_wrapper.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 . /spack/apps/lmod/8.2/init/bash
 module use /spack/apps/lmod/linux-centos7-x86_64/Core
-module load usc motioncor2
+#temporary to use K40 gpus
+module load usc motioncor2/1.2.3
+#as soon as p100 will be available - switch to this one
+#module load usc motioncor2/1.4.0
 
 nvidia-smi
 
