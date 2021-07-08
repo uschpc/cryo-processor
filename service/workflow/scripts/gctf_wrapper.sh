542f9d1ab9675530984be5fbc7d7b8a3d33e60d4
osinskit <osinskit@gmail.com>
2021-07-08 13:24:44 -0700

    wrapper tweaks

--- service/workflow/scripts/gctf_wrapper.sh
+++ service/workflow/scripts/gctf_wrapper.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 #. /spack/apps/lmod/8.2/init/bash
 #module use /spack/apps/lmod/linux-centos7-x86_64/Core
-module load usc gctf
+#module load usc gctf
+
+
+export PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb/bin:$PATH
+export LD_LIBRARY_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb/lib64:$LIBRARY_PATH
+export CPATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb/include:$CPATH
+export CMAKE_PREFIX_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb:$CMAKE_PREFIX_PATH
+export CUDA_HOME=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb
+export CUDA_ROOT=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb
+
+export PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/gctf-1.18/bin:$PATH
+
 
 gctf "$@"
 
--- service/workflow/scripts/motioncor2_wrapper.sh
+++ service/workflow/scripts/motioncor2_wrapper.sh
@@ -2,12 +2,20 @@
 #. /spack/apps/lmod/8.2/init/bash
 #module use /spack/apps/lmod/linux-centos7-x86_64/Core
 #temporary to use K40 gpus
-module load usc motioncor2/1.2.3
+#module load usc motioncor2/1.2.3
 #as soon as p100 will be available - switch to this one
 #module load usc motioncor2/1.4.0
 
 #nvidia-smi
+export PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.0.130-t6gcqrfrbeyep65wpy6erigjcovs7pjm/bin:$PATH
+export LD_LIBRARY_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.0.130-t6gcqrfrbeyep65wpy6erigjcovs7pjm/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.0.130-t6gcqrfrbeyep65wpy6erigjcovs7pjm/lib64:$LIBRARY_PATH
+export CPATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.0.130-t6gcqrfrbeyep65wpy6erigjcovs7pjm/include:$CPATH
+export CMAKE_PREFIX_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.0.130-t6gcqrfrbeyep65wpy6erigjcovs7pjm:$CMAKE_PREFIX_PATH
+export CUDA_HOME=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.0.130-t6gcqrfrbeyep65wpy6erigjcovs7pjm
+export CUDA_ROOT=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.0.130-t6gcqrfrbeyep65wpy6erigjcovs7pjm
 
+export PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/motioncor2-1.2.3/bin:$PATH
 MotionCor2 "$@"
 
 exit $?
