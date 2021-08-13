862c04f4b5b3cf1a2625c62ad84f1de01d1adea2
osinskit <osinskit@gmail.com>
2021-08-12 23:24:14 -0700

    Update Session.py

--- service/Session.py
+++ service/Session.py
@@ -93,7 +93,7 @@ class Session:
 
     def count_processed_files(self):
         try:
-            pf = self.wf.find_files3(os.path.join(os.path.join(self._session_dir, "processed"),"*DW.mrc")))
+            pf = self.wf.find_files3(os.path.join(os.path.join(self._session_dir, "processed"),"*DW.mrc"))
             log.info("processed files are in: %s"%os.path.join(os.path.join(self._session_dir, "processed")))
             log.info("No. of raw files %i"%len(pf)
             return len(pf)
