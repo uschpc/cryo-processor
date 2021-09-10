de4472ab396c1b7a9fac034cc6482fce9af1cbb8
osinskit <osinskit@gmail.com>
2021-09-10 12:46:32 -0700

    Update slack_notify.sh

--- service/workflow/scripts/slack_notify.sh
+++ service/workflow/scripts/slack_notify.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-img_file_path="$1"
-gctf_output_fn="$2"
-mc2_output_fn="$3"
-slack_notify_out_fn="$4"
+img_file_path=$1
+gctf_output_fn=$2
+mc2_output_fn=$3
+slack_notify_out_fn=$4
 #resolution="$2"
 #shifts="$3"
 
@@ -21,8 +21,8 @@ message="\"Estimated resolution limit: ${resolution}\nMotion correction shifts:\
 echo "${message}" >> $slack_notify_out_fn
 echo "" >> $slack_notify_out_fn
 
-curl --data-urlencode "file=${filepath}" --data-urlencode "\"message=${message}\"" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'
-echo "curl --data-urlencode "file=${filepath}" --data-urlencode "message=${message}" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'" >> $slack_notify_out_fn
+curl --data-urlencode "file=${img_file_path}" --data-urlencode "message=\"${message}\"" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'
+echo "curl --data-urlencode \"file=${img_file_path}\" --data-urlencode \"message=\"${message}\"\" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'" >> $slack_notify_out_fn
 
 exit $?
 
