d89fdc11e87901437264d442a2ef40bbbfab62bd
cryoemadmin <cryoemadmin@knoll.hpc.usc.edu>
2021-09-23 10:40:56 -0700

    using pegasus from home dir

--- service/runme.sh
+++ service/runme.sh
@@ -21,7 +21,8 @@ fi
 
 #export PEGASUS_HOME=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/pegasus-5.0.0-esezn6jgoegtjkugiaacbdotigbgevwu
 #export PEGASUS_HOME=/project/cryoem/software/pegasus-5.0.1dev
-export PEGASUS_HOME=/home1/rynge/software/pegasus-5.0.1dev
+#export PEGASUS_HOME=/home1/rynge/software/pegasus-5.0.1dev
+export PEGASUS_HOME=/home1/cryoemadmin/software/pegasus-5.0.1dev
 export PATH=$PEGASUS_HOME/bin:$PATH
 export PYTHONPATH=$(pegasus-config --python)
 export LANG=en_US.UTF-8
