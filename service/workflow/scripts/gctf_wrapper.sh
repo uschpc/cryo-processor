134a4f37c661f0359d2b4b1acf5a68c166aeb9c6
CryoEM Administrator <cryoemadmin@knoll.hpc.usc.edu>
2021-10-13 15:33:19 -0700

    fixes

--- service/workflow/scripts/gctf_wrapper.sh
+++ service/workflow/scripts/gctf_wrapper.sh
@@ -3,17 +3,21 @@
 #module use /spack/apps/lmod/linux-centos7-x86_64/Core
 #module load usc gctf
 
-
-export PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb/bin:$PATH
-export LD_LIBRARY_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb/lib64:$LD_LIBRARY_PATH
-export LIBRARY_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb/lib64:$LIBRARY_PATH
-export CPATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb/include:$CPATH
-export CMAKE_PREFIX_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb:$CMAKE_PREFIX_PATH
-export CUDA_HOME=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb
-export CUDA_ROOT=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-10.1.243-ti55aznlgai3utc7jwrryt2ykxoiz3bb
+export PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-11.2.0-7uwimxj27s4cptafhkw6a6fpyqf5nw4c/bin:$PATH
+export LD_LIBRARY_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-11.2.0-7uwimxj27s4cptafhkw6a6fpyqf5nw4c/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-11.2.0-7uwimxj27s4cptafhkw6a6fpyqf5nw4c/lib64:$LIBRARY_PATH
+export CPATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-11.2.0-7uwimxj27s4cptafhkw6a6fpyqf5nw4c/include:$CPATH
+export CMAKE_PREFIX_PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-11.2.0-7uwimxj27s4cptafhkw6a6fpyqf5nw4c:$CMAKE_PREFIX_PATH
+export CUDA_HOME=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-11.2.0-7uwimxj27s4cptafhkw6a6fpyqf5nw4c
+export CUDA_ROOT=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/cuda-11.2.0-7uwimxj27s4cptafhkw6a6fpyqf5nw4c
 
 export PATH=/spack/apps/linux-centos7-x86_64/gcc-8.3.0/gctf-1.18/bin:$PATH
 
+
+#provide old cuda libs for gctf to work with newer cuda on a40
+export LD_LIBRARY_PATH=/home1/cryoemadmin/software/gctf_libs_from_c10.1.243_to_work_with_c11.2.0_on_a40:$LD_LIBRARY_PATH
+
+#ldd `which gctf`
 
 gctf "$@"
 
