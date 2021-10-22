08219b19d101314f7d27842aa29a8d3fbc9127ae
osinskit <osinskit@gmail.com>
2021-10-21 18:05:26 -0700

    bugfixing

--- service/workflow/scripts/slack_notify.sh
+++ service/workflow/scripts/slack_notify.sh
@@ -3,7 +3,7 @@ img_file_path=$1
 #gctf_output_fn=$2
 #mc2_output_fn=$3
 #slack_notify_out_fn=$4
-#slack_notify_out_fn=$4
+slack_notify_out_fn=$2
 #resolution="$2"
 #shifts="$3"
 
@@ -33,6 +33,8 @@ img_file_path=$1
 #curl --data-urlencode "file=${img_file_path}" --data-urlencode "message=$(printf 'Estimated resolution limit: *%sÅ*\nMotion correction shifts:\n```%s```\n' "${resolution}" "${shifts}")" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'
 
 curl --data-urlencode "file=${img_file_path}" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'
+
+echo "curl --data-urlencode \"file=${img_file_path}\" -X POST \'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php\'" >> $slack_notify_out_fn
 
 #echo "curl --data-urlencode \"file=${img_file_path}\" --data-urlencode \"message=\"${message}\"\" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'" >> $slack_notify_out_fn
 
