7de391392d5fdc382568039e7d92f769d123c430
osinskit <osinskit@gmail.com>
2021-10-25 12:42:23 -0700

    Update slack_notify.sh

--- service/workflow/scripts/slack_notify.sh
+++ service/workflow/scripts/slack_notify.sh
@@ -34,7 +34,11 @@ slack_notify_out_fn=$2
 
 curl --data-urlencode "file=${img_file_path}" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'
 
+echo "command used during processing:" >> $slack_notify_out_fn
 echo "curl --data-urlencode \"file=${img_file_path}\" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'" >> $slack_notify_out_fn
+echo "command to use for debuggging:" >> $slack_notify_out_fn
+echo "curl --data-urlencode \"file=${img_file_path}\" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'" >> $slack_notify_out_fn
+
 
 #echo "curl --data-urlencode \"file=${img_file_path}\" --data-urlencode \"message=\"${message}\"\" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'" >> $slack_notify_out_fn
 
