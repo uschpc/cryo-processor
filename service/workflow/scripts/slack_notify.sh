1837cfe8292feb4783937618d04e3140b64093d9
osinskit <osinskit@gmail.com>
2022-07-21 12:12:32 -0700

    a

--- service/workflow/scripts/slack_notify.sh
+++ service/workflow/scripts/slack_notify.sh
@@ -9,6 +9,7 @@ echo "curl -k --data-urlencode \"file=${img_file_path}\" -X POST 'https://hpcacc
 echo "command to use for debuggging:" >> $slack_notify_out_fn
 echo "curl -k --data-urlencode \"file=${img_file_path}\" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'" >> $slack_notify_out_fn
 #echo "curl -k --data-urlencode \"file=${img_file_path}\" --data-urlencode \"message=\"${message}\"\" -X POST 'https://hpcaccount.usc.edu/public/cryoem/slack/postimage.php'" >> $slack_notify_out_fn
+echo "Sent at: `date`" >> $slack_notify_out_fn
 
 exit $?
 
