a53d1a79b8a4c33065331762ef2767833c8808b7
osinskit <osinskit@gmail.com>
2022-08-02 15:52:11 -0700

    Update PipelineWorkflow.py

--- service/PipelineWorkflow.py
+++ service/PipelineWorkflow.py
@@ -500,8 +500,8 @@ class PipelineWorkflow:
             #if fastcounter % 30 == 0:
             #    slowcounter+=1
             for fraction_file_path in element:
-                #ffp_index = element.index(fraction_file_path)
-                ffp_index = 0
+                ffp_index = element.index(fraction_file_path)
+                #ffp_index = 0
                 
                 fraction_file_name = os.path.basename(fraction_file_path)
                 fraction_file = File(fraction_file_name)
@@ -529,7 +529,7 @@ class PipelineWorkflow:
                 else:
                     logger.info("Unknown image extension - {}".format(self.basename_extension))
                     sys.exit(1)
-                mc_cmd0="{} {} -OutMrc {} -Iter 7 -Tol 0.5 -Kv {} -PixSize {} -FmDose {} -Serial 0 -OutStack 0 -SumRange 0 0 -GpuMemUsage 0.2 -Gpu {}"
+                mc_cmd0="{} {} -OutMrc {} -Iter 7 -Tol 0.5 -Kv {} -PixSize {} -FmDose {} -Serial 0 -OutStack 0 -SumRange 0 0 -GpuMemUsage 0.4 -Gpu {}"
                 mc_cmd1=mc_cmd0+" -Gain {} -Throw {} -Trunc {}"
                 mc_cmd2=mc_cmd0+" -Gain {}"
                 mc_cmd3=mc_cmd0+" -Throw {} -Trunc {}"
