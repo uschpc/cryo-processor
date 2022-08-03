743ad840e449a7d1a82d1f77bf04b516bc5d9b1e
osinskit <osinskit@gmail.com>
2022-08-03 14:01:56 -0700

    Update e2proc2d2_wrapper_dual.sh

--- service/workflow/scripts/e2proc2d2_wrapper_dual.sh
+++ service/workflow/scripts/e2proc2d2_wrapper_dual.sh
@@ -7,8 +7,8 @@ file0_out=$2
 file1_in=$3
 file1_out=$4
 
-e2proc2d.py --process=filter.lowpass.gauss:cutoff_freq=0.1 --fixintscaling=sane $file0_in $file0_out & PIDONE=$!
-e2proc2d.py --process=filter.lowpass.gauss:cutoff_freq=0.1 --fixintscaling=sane $file1_in $file1_out & PIDTWO=$!
+e2proc2d.py --fixintscaling=sane $file0_in $file0_out & PIDONE=$!
+e2proc2d.py --fixintscaling=sane $file1_in $file1_out & PIDTWO=$!
 
 wait $PIDONE
 wait $PIDTWO
