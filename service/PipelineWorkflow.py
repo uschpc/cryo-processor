d066ddd91cb8079001d74b3a17686cdc04b81957
osinskit <osinskit@gmail.com>
2022-10-30 20:57:29 -0700

    Update PipelineWorkflow.py

--- service/PipelineWorkflow.py
+++ service/PipelineWorkflow.py
@@ -660,6 +660,9 @@ class PipelineWorkflow:
         #self.tc.add_transformations(copy_jpeg) # cannot be used; might be removed soon
         #single gpu
         self.tc.add_transformations(motionCor2)
+        self.tc.add_transformations(motionCor2_g)
+        self.tc.add_transformations(motionCor2_gtt)
+        self.tc.add_transformations(motionCor2_tt
         self.tc.add_transformations(gctf)
         self.tc.add_transformations(e2proc2d)
         self.tc.add_transformations(e2proc2d2)
