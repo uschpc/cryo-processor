a1872791f496dc04f2f10dbc432f611e29eb1d0b
osinskit <osinskit@gmail.com>
2023-01-02 07:46:35 -0800

    conda path update

--- service/workflow/scripts/e2proc2d_wrapper.sh
+++ service/workflow/scripts/e2proc2d_wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. "/spack/apps/linux-centos7-x86_64/gcc-8.3.0/anaconda3-2019.10-bpb6unkhyvkirwkg44uqchcy5jyhzhvt/etc/profile.d/conda.sh"
+. "/spack/conda/miniconda3/4.12.0/etc/profile.d/conda.sh"
 conda activate /spack/apps/linux-centos7-x86_64/gcc-8.3.0/cryoem/eman2
 
 e2proc2d.py "$@"
